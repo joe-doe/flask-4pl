17b1cb47631ea51562dca2788c67ef04b5dfc459
joe-doe <evangelos.r@tutanota.com>
2019-03-03 18:04:47 +0200

    Update flask-4pl.py

--- flask-4pl.py
+++ flask-4pl.py
@@ -1,8 +1,9 @@
 import io
 import random
-from flask import Flask, render_template, Response, url_for
+from flask import Flask, render_template, Response
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
+from matplotlib.pyplot import figure
+import core
 
 app = Flask(__name__)
 
@@ -14,14 +15,14 @@ def index():
 
 @app.route('/plot.png')
 def plot_png():
-    fig = create_figure()
+    fig = core.make_4pl()
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)
     return Response(output.getvalue(), mimetype='image/png')
 
 
 def create_figure():
-    fig = Figure()
+    fig = figure()
     axis = fig.add_subplot(1, 1, 1)
     xs = range(100)
     ys = [random.randint(1, 50) for x in xs]
